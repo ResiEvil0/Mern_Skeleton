ee4039baba5e8a1bff23497fa57cab56d4759089
ResiEvil0 <56939391+ResiEvil0@users.noreply.github.com>
2021-06-04 17:10:01 -0400

    Add files via upload

---
+++ server/express.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compress from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
+import template from './../template'
+
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(compress())
+// secure apps by setting various HTTP headers
+app.use(helmet())
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.status(200).send(template())
+})
+
+app.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+      res.status(401).json({"error" : err.name + ": " + err.message})
+    }else if (err) {
+      res.status(400).json({"error" : err.name + ": " + err.message})
+      console.log(err)
+    }
+  })
+
+export default app
